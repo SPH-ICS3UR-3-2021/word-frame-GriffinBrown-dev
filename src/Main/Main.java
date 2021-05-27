605ca6e8ce6af5000bddd3e9a96dcd746dc658b5
Griffin Brown <82894106+GriffinBrown-dev@users.noreply.github.com>
2021-05-26 22:43:14 -0400

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package Main;
+import java.util.Scanner;
 
 /**
  *
@@ -11,7 +12,26 @@ package Main;
  */
 public class Main {
     public static void main(String[] args){
-        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a word.");
+        String word = in.nextLine();
+        String firstLine = "";
+        for (int i = 0; i < word.length(); i++) {
+            firstLine = firstLine + word.charAt(i) + " ";
+        }
+        System.out.println("* " + firstLine + "*");
+        for (int a = 0; a < word.length(); a++) {
+            String middleLine = "";
+            for (int b = 0; b < word.length(); b++) {
+                middleLine = middleLine + "* ";
+            }
+            System.out.println(word.charAt(word.length()-1-a) + " " + middleLine + word.charAt(a));
+        }
+        String lastLine = "";
+        for (int i = word.length()-1; i >= 0; i--) {
+            lastLine = lastLine + word.charAt(i) + " ";
+        }
+        System.out.println("* " + lastLine + "*");
     }
     
 }
